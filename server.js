9c10a20ce7ae3ee8c645a7f001dd7d89fa637dfc
Guo Yu <i@guoyu.me>
2013-10-25 16:35:26 +0800

    split middlewares

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-// @brief  : [project brief]
-// @author : [project author](project site)
-
 var express = require('express'),
     http = require('http'),
     path = require('path'),
     MongoStore = require('connect-mongo')(express),
-    pkg = require('./lib/pkg'),
+    less = require('less-middleware'),
+    json = require('./libs/json'),
     sys = require('./package.json'),
-    errors = require('./lib/error');
+    errors = require('./middlewares/error');
 
 var Server = function(params) {
 
@@ -16,39 +14,29 @@ var Server = function(params) {
         var app = express(),
             self = this;
 
-        pkg.set(path.join(__dirname, '/database.json'), params.database);
+        if (params.database) json.save(path.join(__dirname, '/database.json'), params.database);
+        if (params.database && params.database.name) secret = params.database.name;
 
         // routers
         var index = require('./routes/index'),
             sign = require('./routes/sign');
 
         // all environments
-        app.set('env', params.env ? params.env : 'development');
-        app.set('views', __dirname + '/views');
+        app.set('env', params.env && typeof(params.env) == 'string' ? params.env : 'development');
+        app.set('views', path.join(__dirname, '/views'));
         app.set('view engine', 'jade');
-        app.use(express.favicon());
+        app.use(express.favicon(path.join(__dirname, '/public/images/favicon.ico')));
         app.use(express.logger('dev'));
-        app.use(express.bodyParser({
-            keepExtensions: true,
-            uploadDir: path.join(__dirname, '/public/uploads')
-        }));
+        app.use(express.bodyParser({ keepExtensions: true, uploadDir: path.join(__dirname, '/public/uploads') }));
         app.use(express.methodOverride());
-        app.use(express.cookieParser(params.database.name));
-        app.use(express.session({
-            secret: params.database.name,
-            store: new MongoStore({
-                db: params.database.name,
-                collection: 'sessions'
-            })
-        }));
+        app.use(express.cookieParser(secret));
+        app.use(express.session({ secret: secret, store: new MongoStore({ db: secret, collection: 'sessions' }) }));
+        app.use(less({ src: path.join(__dirname, 'public') }));
+        app.use(express.static(path.join(__dirname, 'public')));
         app.use(function(req, res, next) {
             if (!res.locals._app) res.locals._app = self.app;
             next();
         });
-        app.use(require('less-middleware')({
-            src: __dirname + '/public'
-        }));
-        app.use(express.static(path.join(__dirname, 'public')));
         app.use(app.router);
 
         // locals
@@ -70,14 +58,19 @@ var Server = function(params) {
         self.params = params;
 
     }
-
+    return this;
 }
 
 Server.prototype.run = function(port) {
     var defaultPort = 3000;
-    this.app.set('port', (port && !isNaN(parseInt(port, 10))) ? parseInt(port, 10) : defaultPort);
-    this.app.locals.root = (this.app.get('env') === 'production') ? this.params.url : 'http://localhost:' + this.app.get('port');
-    http.createServer(this.app).listen(this.app.get('port'));
+    if (this.app) {
+        this.app.set('port', (port && !isNaN(parseInt(port, 10))) ? parseInt(port, 10) : defaultPort);
+        this.app.locals.url = (this.app.get('env') === 'production') ? this.params.url : 'http://localhost:' + this.app.get('port');
+        http.createServer(this.app).listen(this.app.get('port'));
+        return this;
+    } else {
+        return false;
+    }
 }
 
 module.exports = Server;
