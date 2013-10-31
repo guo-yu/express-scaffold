8579ec06a0cda235d89c89a029823d992d1f8a1b
Guo Yu <i@guoyu.me>
2013-10-31 16:55:18 +0800

    添加注释

--- routes/home.js
+++ routes/home.js
@@ -6,24 +6,45 @@
 // PUT     /:id       ->  update
 // DELETE  /:id       ->  destroy
 
+/**
+ * GET index.
+ */
 exports.index = function(req, res, next) {
     res.send('hi, It just works!');
 }
+/**
+ * GET new.
+ */
 exports.new = function(req, res, next) {
     res.send('new');
 }
+/**
+ * POST create.
+ */
 exports.create = function(req, res, next) {
     res.send('create');
 }
+/**
+ * GET show.
+ */
 exports.show = function(req, res, next) {
     res.send('show');
 }
+/**
+ * GET edit.
+ */
 exports.edit = function(req, res, next) {
     res.send('edit');
 }
+/**
+ * PUT update.
+ */
 exports.update = function(req, res, next) {
     res.send('update!');
 }
+/**
+ * DELETE destroy.
+ */
 exports.destroy = function(req, res, next) {
     res.send('destroy!');
 }
