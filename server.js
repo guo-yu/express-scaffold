d3d636d526e4d617722b00387bee8a21a52f1790
Guo Yu <i@guoyu.me>
2013-12-19 15:34:49 +0800

    0.2.0: server.js cleaned

--- server.js
+++ server.js
@@ -11,46 +11,51 @@ var http = require('http'),
     middlewares = require('./middlewares/index'),
     routes = require('./routes/index');
 
-var parentPath = function(dir) {
-    return path.resolve(__dirname , '../../', dir);
-};
-
 var defaults = {
     port: 3000,
     env: 'development',
+    'view engine': 'jade',
     views: path.join(__dirname, '/views'),
-    public: path.join(__dirname, 'public'),
+    public: path.join(__dirname, '/public'),
     uploads: path.join(__dirname, '/public/uploads'),
     uploadsLimit: '20mb',
-    'view engine': 'jade',
-    log: ":remote-addr|:date|:method|:url|:status|:res[content-length]|:response-time|\":referrer\"|\":user-agent\""
+    log: ":remote-addr|:date|:method|:url|:status|:res[content-length]|:response-time|\":referrer\"|\":user-agent\"",
+    database: {
+        name: sys.name
+    },
+    session: {
+        secret: sys.name
+    }
+};
+
+var dirfinder = function(dir) {
+    if (dir) return path.resolve(__dirname , '../../', dir);
+    return defaults[dir];
 };
 
 var Server = function(configs) {
 
     var app = express(),
-        params = configs ? configs : {},
-        secret = params.database && params.database.name ? params.database.name : sys.name,
-        mongoStore = new MongoStore({ db: secret });
+        settings = _.extend(defaults, configs);
 
-    if (params.database) json.save(path.join(__dirname, '/configs/database.json'), params.database);
+    if (settings.database) json.save(path.join(__dirname, '/configs/database.json'), settings.database);
+    if (settings.session.store) settings.session.store = new MongoStore({ db: settings.session.secret });
 
     // all environments
-    app.set('port', (params.port && !isNaN(parseInt(params.port, 10))) ? parseInt(params.port, 10) : defaults.port);
-    app.set('env', params.env ? params.env : defaults.env);
-    app.set('views', params.views ? parentPath(params.views) : defaults.views);
-    app.set('view engine', params['view engine'] ? params['view engine'] : defaults['view engine']);
-    app.set('log', params.log ? params.log : defaults.log);
+    app.set('env', settings.env);
+    app.set('port', _.isNumber(parseInt(settings.port)) ? parseInt(settings.port) : defaults.port);
+    app.set('views', dirfinder(configs.views));
+    app.set('view engine', settings['view engine']);
     app.use(express.favicon());
-    app.use(express.logger(app.get('env') !== 'production' ? 'dev' : app.get('log')));
+    app.use(express.logger('production' !== settings.env ? 'dev' : settings.log));
     app.use(express.compress());
-    app.use(express.limit(params.uploadsLimit ? params.uploadsLimit : defaults.uploadsLimit));
-    app.use(express.bodyParser({keepExtensions: true, uploadDir: params.uploads ? parentPath(params.uploads) : defaults.uploads}));
+    app.use(express.limit(settings.uploadsLimit));
+    app.use(express.bodyParser({keepExtensions: true, uploadDir: configs.uploads ? parentPath(configs.uploads) : settings.uploads}));
     app.use(express.methodOverride());
-    app.use(express.cookieParser(secret));
-    app.use(express.session({ secret: secret, store: mongoStore }));
-    app.use(less({src: params.public ? parentPath(params.public) : defaults.public }));
-    app.use(express.static(params.public ? parentPath(params.public) : defaults.public ));
+    app.use(express.cookieParser(settings.session.secret));
+    app.use(express.session(settings.session));
+    app.use(less({src: dirfinder(configs.public) }));
+    app.use(express.static(dirfinder(configs.public)));
     app.use(app.router);
 
     // errors
@@ -60,14 +65,13 @@ var Server = function(configs) {
 
     // locals
     app.locals.sys = sys;
-    app.locals.site = params;
-    app.locals.url = ('production' === app.get('env')) ? params.url : 'http://localhost:' + app.get('port');
+    app.locals.site = settings;
+    app.locals.url = ('production' === settings.env) ? settings.url : 'http://localhost:' + app.get('port');
 
-    this.params = params;
     this.app = app;
     this.deps = new Depender;
-    this.deps.define('$mongoStore', mongoStore);
     this.deps.define('$middlewares', middlewares);
+    if (settings.session.store) this.deps.define('$sessionStore', settings.session.store);
 
     return this;
 }
@@ -106,7 +110,7 @@ Server.prototype.routes = function(init) {
 Server.prototype.run = function(port) {
     var app = this.app;
     if (_.isEmpty(app.routes)) this.routes();
-    if (port && !isNaN(parseInt(port, 10))) app.set('port', parseInt(port, 10));
+    if (port && _.isNumber(parseInt(port))) app.set('port', parseInt(port));
     http.createServer(app).listen(app.get('port'));
     return this;
 }
