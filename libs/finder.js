b1b2f9b969c96011aabb77bcd8e993f0372b8e07
Guo Yu <i@guoyu.me>
2014-06-12 16:12:11 +0800

    0.3.9: bugfix, blank configs object error

--- libs/finder.js
+++ libs/finder.js
@@ -1,8 +1,9 @@
 var path = require('path');
+var _ = require('underscore');
 var defaults = require('../configs');
 var parent = path.resolve(__dirname, '../../../');
 
 module.exports = function(configs, key) {
-  if (!configs[key]) return defaults[key]; 
+  if (!configs || !_.isObject(configs) || !configs[key]) return defaults[key]; 
   return path.join(parent, configs[key]);  
 }
