b16e9262f44b623ee8dfa749627b4f6a3df64c4c
Guo Yu <i@guoyu.me>
2015-01-12 14:16:28 +0800

    Rewriting lib server

--- libs/server.js
+++ libs/server.js
@@ -1,58 +1,42 @@
-/**
-*
-* Global dependencies
-*
-**/
 var _ = require('underscore');
+var depender = require('depender');
+
+// Express core dependencies
 var express = require('express');
 var logger = require("morgan");
-var compress = require('compression');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var multer  = require('multer');
+var bodyParser = require('body-parser'); 
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var depender = require('depender');
-var less = require('less-middleware');
-var sass = require('node-sass').middleware;
-var mongoStore = require('connect-mongo')({ session: session });
 
-/**
-*
-* Local dependencies
-*
-**/
-var pkg = require('../package');
+// Local modules
+var pkg = require('../package.json');
+var utils = require('./utils');
 var finder = require('./finder');
 var debug = require('./debug');
 var defaults = require('../configs');
 
-var dbs = {};
-dbs.mongodb = require('../dbs/mongodb');
+var databases = {};
+databases.mongodb = require('../databases/mongodb');
 
-var ctrlers = {};
-ctrlers.mongoose = require('../ctrlers/mongoose');
+var controllers = {};
+controllers.mongoose = require('../controllers/mongoose');
 
 var middlewares = {};
 middlewares.passport = require('express-passport');
 middlewares.errors = require('express-common-errors');
 
-/**
-*
-* Expose main function
-*
-**/
+// Load optional dependencies
+var dependencies = utils.bootstrap();
+
+// Expose main function
 module.exports = Server;
 
-/**
-*
-* Server Class
-* @configs[Object]: the config object, 
-* checkout `./configs/default.js` for more infomation.
-*
-**/
-function Server(configs) {
+// var mongoStore = require('connect-mongo')({ session: session });
 
+/**
+ * [Server Class]
+ * @param {Object} configs [the config object]
+ */
+function Server(configs) {
   var dirs = {};
   var app = express();
 
@@ -60,7 +44,9 @@ function Server(configs) {
   var devMode = !(env === 'production');
 
   var settings = _.extend(_.clone(defaults), configs || {});
-  var dbname =  dbs.mongodb.isMongodbUri(settings.database) ? dbs.mongodb.parseDbname(settings.database) : settings.database.name;
+  var dbname =  databases.mongodb.isMongodbUri(settings.database) ? 
+    databases.mongodb.parseDbname(settings.database) : 
+    settings.database.name;
       
   if (settings.session.store) {
     var host = settings.session.host || 'localhost';
@@ -70,107 +56,120 @@ function Server(configs) {
     settings.session.store = new mongoStore({ db: dbname, host: host, port: port, username: username, password: password });
   }
 
-  if (!settings.session.secret) {
+  if (!settings.session.secret)
     settings.session.secret = dbname;
-  }
 
-  // find `views` and `public` abs path
+  // Find `views` and `public` absolute path
   dirs.views = finder(configs, 'views');
   dirs.publics = finder(configs, 'publics');
   dirs.uploads = finder(configs, 'uploads');
 
-  // setup express settings
+  // Setup express settings
   app.set('env', env);
   app.set('views', dirs.views);
   app.set('view engine', settings['view engine']);
   app.set('port', process.env.PORT || 3000);
-  if (devMode) app.set('view cache', false);
 
-  // load all middlewares
+  if (devMode) 
+    app.set('view cache', false);
+
+  // Load express middlewares
   app.use(logger(devMode ? 'dev' : settings.logformat));
-  app.use(compress());
+
+  if (dependencies.compression)
+    app.use(dependencies.compression());
+
   app.use(bodyParser());
-  app.use(multer({ dest: dirs.uploads }));
-  app.use(methodOverride());
+
+  if (dependencies.multer)
+    app.use(dependencies.multer({ dest: dirs.uploads }));
+
+  if (dependencies['method-override'])
+    app.use(dependencies['method-override'](settings['method-override'] || {}));
+
   app.use(cookieParser(settings.session.secret));
-  app.use(session(settings.session));
-  app.use(less(dirs.publics));
-  app.use(sass({ src: dirs.publics }));
+
+  if (dependencies['express-session'])
+    app.use(dependencies['express-session'](settings.session));
+
+  if (dependencies['less-middleware'])
+    app.use(dependencies['less-middleware'](dirs.publics));
+
+  if (dependencies['node-sass-middleware'])
+    app.use(dependencies['node-sass-middleware']({ src: dirs.publics }));
+
   app.use(express.static(dirs.publics));
 
-  // expose locals to template engine
+  // Expose locals to template engine
   app.locals.sys = pkg;
   app.locals.site = settings;
   app.locals.settings = settings;
-  app.locals.url = devMode ? 'http://127.0.0.1:' + app.get('port') : settings.url
+  app.locals.url = devMode ? 'http://127.0.0.1:' + app.get('port') : settings.url;
+  app.locals.uri = app.locals.url;
 
   this.app = app;
   this.deps = new depender;
   this.deps.define('debug', debug(settings.name));
   this.deps.define('express', express);
   this.deps.define('middlewares', middlewares);
+
   this.settings = settings;
 
-  if (settings.session.store) {
+  if (settings.session.store)
     this.deps.define('sessionStore', settings.session.store);
-  }
 
   return this;
 }
 
 /**
-*
-* Define data models
-* @init[Function]: the callback function to return model object.
-*
-**/
-Server.prototype.models = function(models) {
-  this.deps.define('Schema', dbs.mongodb.Schema);
-  this.deps.define('db', dbs.mongodb.connect(this.settings.database));
-  this.deps.define('models', this.deps.use(models));
+ * [Define data models]
+ * @param  {Function} fn [the callback function to return model object]
+ */
+Server.prototype.models = function(fn) {
+  this.deps.define('Schema', databases.mongodb.Schema);
+  this.deps.define('db', databases.mongodb.connect(this.settings.database));
+  this.deps.define('models', this.deps.use(fn));
   return this;
 }
 
 /**
-*
-* Define spec Ctrlers
-* @init[Function]: the callback function to return spec ctrlers.
-*
-**/
-Server.prototype.ctrlers = function(controllers) {
-  this.deps.define('Ctrler', ctrlers.mongoose);
-  this.deps.define('ctrlers', this.deps.use(controllers));
+ * [Init Controllers instances]
+ * @param  {Function} fn [the callback function to return spec controllers]
+ */
+Server.prototype.controllers = function(fn) {
+  this.deps.define('Controller', controllers.mongoose);
+  this.deps.define('controllers', this.deps.use(fn));
   return this;
 }
 
 /**
-*
-* Define routes
-* @init[Function]: the callback function to inject routes into `app`.
-*
-**/
-Server.prototype.routes = function(routes) {
+ * [Init routes]
+ * @param  {Function} fn [index router]
+ */
+Server.prototype.routes = function(fn) {
   this.deps.define('app', this.app);
-  this.deps.use(routes);
+  this.deps.use(fn);
+
   this.app.use(middlewares.errors.logger);
   this.app.use(middlewares.errors.xhr);
   this.app.use(middlewares.errors.common);
+
   this.app.all('*', middlewares.errors.notfound); // 404
   return this;
 }
 
-/**
-*
-* Start server instance
-*
-**/
+// Start server instance
 Server.prototype.run = function() {
   var app = this.app;
   var log = debug(this.settings.name)('http');
 
-  log('is running on : %s', app.locals.url);
-  log('NODE_ENV      : %s', app.get('env'));
-  log('PORT          : %s', app.get('port'));
+  log(
+    '%s is running [%s][%s] @ %s', 
+    this.settings.name, 
+    app.locals.url,
+    app.get('port'),
+    app.get('env')
+  );
 
   return app.listen(app.get('port'));
 }
