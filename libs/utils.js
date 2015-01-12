4bee8d27163023cde217a1053f694d96a6e75bcb
Guo Yu <i@guoyu.me>
2015-01-12 15:27:05 +0800

    Add utils.camelize function

--- libs/utils.js
+++ libs/utils.js
@@ -1,34 +1,46 @@
 var debug = require('./debug');
 
-exports.gatheringInformation = gatheringInformation;
-exports.isLoadError = isLoadError;
-
-function gatheringInformation(moduleName) {
-  try {
-    var module = require(moduleName);
-    return module;
-  } catch (err) {
-    debug('utils')(err);
-    return 'No ' + moduleName + ' module found, aborted.'
-  }
-}
-
-function isLoadError(name) {
-  return typeof(name) === 'string';
-}
+exports.bootstrap = bootstrap;
 
 function bootstrap() {
   var pkg = require('../package.json');
   var modules = {};
 
   Object.keys(pkg.devDependencies).forEach(function(moduleName) {
-    var m = gatheringInformation(moduleName);
+    var m = loadModule(moduleName);
 
-    if (isLoadError(m))
+    if (typeof(m) === 'string')
       return debug('bootstrap')(m);
 
-    modules[moduleName] = m;
+    modules[camelize(moduleName)] = m;
   });
 
   return modules;
+}
+
+function loadModule(moduleName) {
+  try {
+    return require(moduleName);;
+  } catch (err) {
+    debug('utils')(err);
+    return 'No ' + moduleName + ' module found, aborted.'
+  }
+}
+
+function camelize(str) {
+  if (str.indexOf('-') === 0)
+    return str;
+  var token = str.split('-');
+  if (!token.length)
+    return str;
+
+  for (var i = token.length - 1; i >= 0; i--) {
+    (function(index){
+      if (index === 0)
+        return;
+      token[index] = token[index].charAt(0).toUpperCase() + token[index].substr(1)
+    })(i);
+  };
+
+  return token.join('');
 }
