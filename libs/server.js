6dcd3621e4eae15a0265f3b30dab6e758dfd9239
Guo Yu <i@guoyu.me>
2015-01-12 17:34:37 +0800

    Format lib server

--- libs/server.js
+++ libs/server.js
@@ -100,7 +100,7 @@ function Server(configs) {
   // Parse `application/x-www-form-urlencoded`
   app.use(bodyParser.urlencoded({ extended: false }));
   // Parse `application/json`
-  app.use(bodyParser.json())
+  app.use(bodyParser.json());
 
   // Upload handler
   if (dependencies.multer)
